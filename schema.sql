80abaa003a785d75abc143c3a73da185a3412732
Ryan Perry <rsperry857@gmail.com>
2023-03-24 12:35:56 -0400

    Renamed app/Added a chunk of playlists

--- schema.sql
+++ schema.sql
@@ -13,8 +13,71 @@ CREATE TABLE Champions(
 );
 
 INSERT INTO Champions(champ_id, champ_name, playlist1)
-    VALUES (147, 'Seraphine', 'https://open.spotify.com/playlist/36Nu75l6mmj0cil6VLxSx0?si=eb8cd6ec3bcf46d4');
+    VALUES (266, 'Aatrox', 'https://open.spotify.com/playlist/0UAl0DYfTlvL0R9HnJHFDX?si=b4af5bf43aee4849')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (103, 'Ahri', 'https://open.spotify.com/playlist/3awLzjKg0buBCcuVphDSwv?si=8477942d94704236')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (84, 'Akali', 'https://open.spotify.com/playlist/0qM0bGFUzhtPtSgX6wcd78?si=9f1f6702c2334a2c')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (166, 'Akshan', 'https://open.spotify.com/playlist/5wBx91p9FsmLEwV93jYvkl?si=73f0d002005448b1')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (12, 'Alistar', 'TEMP')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (32, 'Amumu', 'https://open.spotify.com/playlist/4PehUDLnYqwyXcV28hsxku?si=7bc06f6c8bb4456f')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (34, 'Anivia', 'https://open.spotify.com/playlist/1kR8XcZOi2qXHNjBkubLjI?si=7068271dba5d4bad')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (1, 'Annie', 'https://open.spotify.com/playlist/6IDD6ryfvEkfitvtCcc89T?si=a138293c50664cc1')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (523, 'Aphelios', 'https://open.spotify.com/playlist/4dI4zY4OTOogtGBk39c0jg?si=dd53b3eb2bfe4640')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (22, 'Ashe', 'https://open.spotify.com/playlist/708uwgslpkfyFqbu0tPZG8?si=2c693c46187e41b5')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (136, 'Aurelion Sol', 'https://open.spotify.com/playlist/67XzsPJpduVL9S4Scrqd9m?si=37c1f8c840564a49')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (268, 'Azir', 'https://open.spotify.com/playlist/2I3KMkywmHQv8q3gH4hgGv?si=e37c2eaedc0c4621')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (432, 'Bard', 'https://open.spotify.com/playlist/6i1C6h3XS9qdNMJTPKqFIT?si=1747d9e9391441b3')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (200, "Bel'Veth", 'https://open.spotify.com/playlist/2ehyUPqfi0oJNUbxBrd7bz?si=05eadedbd24743e1')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (53, 'Blitzcrank', 'https://open.spotify.com/playlist/03kLBNuhGTP7q7sIsjWSAl?si=12bd269ba1fa4d94')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (63, 'Brand', 'https://open.spotify.com/playlist/5HYBVTSesLRWSh5iGWul1G?si=508e07481e18462c')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (201, 'Braum', 'https://open.spotify.com/playlist/2JsWcUO9EJlV0RT5A3yWE0?si=ca9e865219094144')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (51, 'Caitlyn', 'https://open.spotify.com/playlist/3N53Zr1rixBHkavuGLl7y1?si=69f3c25694b74564')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (69, 'Cassiopeia', 'https://open.spotify.com/playlist/2XZybNmNAkBqFYq0xlFFNh?si=c3a2c9a88b684e44')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (31, "Cho'Gath", 'https://open.spotify.com/playlist/33qcm84ULYzQjI5lQTMSJB?si=5d18fd2dd2394026')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (42, 'Corki', 'TEMP')
 
 INSERT INTO Champions(champ_id, champ_name, playlist1)
     VALUES (222, 'Jinx', 'https://open.spotify.com/playlist/0zOi2Fe13McYfJm4TIhfzD?si=c52bfb296a59410d')
+
+INSERT INTO Champions(champ_id, champ_name, playlist1)
+    VALUES (147, 'Seraphine', 'https://open.spotify.com/playlist/36Nu75l6mmj0cil6VLxSx0?si=eb8cd6ec3bcf46d4');
 
