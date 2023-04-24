5bec578cfb79aa1402278992d55342919f4c87be
Ryan Perry <rsperry857@gmail.com>
2023-04-24 17:31:25 -0400

    Added OAuth

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -6,13 +6,36 @@ import time
 import main
 
 username = 'TuneTerra'
-clientID = '4fe47df343244305b0c182bf3256a014'
-clientSecret = '44d7366723e44e0b80661de489b0b521'
-redirectURI = 'add frontend url here later'
+clientID = ''
+clientSecret = ''
+redirectURI = ''
 scope = "user-read-playback-state"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 client_sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+authorization_base_url = "https://accounts.spotify.com/authorize"
+token_url = "https://accounts.spotify.com/api/token"
+
+
+from requests_oauthlib import OAuth2Session
+spotify = OAuth2Session(clientID, scope=scope, redirect_uri=redirectURI)
+
+authorization_url, state = spotify.authorization_url(authorization_base_url)
+print('Please go here and authroize: ', authorization_url)
+
+redirect_response = input('\n\nPaste the full redirect URL here: ')
+
+from requests.auth import HTTPBasicAuth
+code = redirect_response[len(redirectURI + 5):]
+
+for i in range(len(code)):
+    if code[i] == '&':
+        code = code[:i]
+        break
+
+auth =  HTTPBasicAuth(clientID, clientSecret)
+full_token = spotify.fetch_token(token_url, auth=auth, code=code)
 
 
 def addSongtoQ(client: spotipy.Spotify, track_uri):
@@ -48,6 +71,3 @@ def playbackController(client: spotipy.Spotify, playlist):
         return
     else:
         return
-
-
-
